8799f1b02ddca42cd750c79197e4e8dbaf1742d3
Danil Onopa <onopa.danil@yandex.ru>
2023-06-26 23:08:48 +0600

    Lab0

---
+++ Lab0/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Lab0
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string? input;
+            do
+            {
+                Console.Clear();
+                Console.Write("Введите год: ");
+                input = Console.ReadLine();
+            } while (string.IsNullOrEmpty(input));
+            
+            int year;
+            try
+            {
+                year = int.Parse(input);
+                Console.WriteLine($"{year} - {(YearService.IsItLeapYear(year) ? "" : "не")}високосный год");
+            }
+            catch (FormatException err)
+            {
+                Console.WriteLine("Введен текст, а не число");
+                return;
+            }
+            catch (ArgumentException err)
+            {
+                Console.WriteLine("Год < 0");
+                return;
+            }
+
+            Console.ReadKey();
+            string? temperature;
+            string? scaleOfTemperature;
+            do
+            {
+                Console.Clear();
+                Console.Write("Введите значение температуры: ");
+                temperature = Console.ReadLine();
+                Console.Write("Введите значение шкалы: ");
+                scaleOfTemperature = Console.ReadLine();
+            } while (string.IsNullOrEmpty(temperature) || string.IsNullOrEmpty(scaleOfTemperature));
+
+            try
+            {
+                Console.WriteLine
+                    ("Результат: " + 
+                     TemperatureService.ConvertTemperature(int.Parse(temperature), scaleOfTemperature));
+            }
+            catch (FormatException err)
+            {
+                Console.WriteLine("Введен текст, а не число");
+            }
+            catch (ArgumentException err)
+            {
+                Console.WriteLine("Введена неверная температурная шкала");
+            }
+        }
+    }
+}
---
+++ Lab0/YearService.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab0
+{
+    public class YearService
+    {
+        public static bool IsItLeapYear(int year)
+        {
+            if (year < 0) throw new ArgumentException("year < 0");
+            if (year % 4 == 0 && year % 100 != 0) return true;
+            return year % 400 == 0;
+        }
+    }
+}
